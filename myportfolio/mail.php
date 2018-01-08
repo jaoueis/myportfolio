03285bf6f6bc14e8c2cc6a832ed9b1b002c485b4
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-01-09 00:14:36 +0800

    set up back end

---
+++ myportfolio/mail.php
@@ -0,0 +1,41 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mysql_host     = "localhost";
+    $mysql_username = "root";
+    $mysql_password = "root";
+    $mysql_database = "db_visitors";
+    $port           = "8889";
+
+    $name    = $_POST['user_name'];
+    $email   = $_POST['user_email'];
+    $message = $_POST['user_message'];
+
+    if (empty($name)) {
+        die("Please enter your name");
+    }
+    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die("Please enter valid email address");
+    }
+
+    if (empty($message)) {
+        die("Please enter text");
+    }
+
+    print "Hello " . $name . "! I have received your message and email " . $email . ".";
+    print " I will contact you very soon!";
+
+    $mysqli = new mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database, $port);
+
+    if ($mysqli->connect_error) {
+        die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+    }
+
+    $statement = $mysqli->prepare("INSERT INTO users_data (user_name, user_email, user_message) VALUES(?, ?, ?)");
+    $statement->bind_param('sss', $name, $email, $message);
+
+    if ($statement->execute()) {
+        print " Hello " . $name . "! your message has been saved!";
+    } else {
+        print $mysqli->error; //show mysql error if any
+    }
+}
