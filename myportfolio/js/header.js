38b13ef629d80bc6f2944d91875e9148685f9a31
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-01-10 00:18:31 +0800

    set up website

--- myportfolio/js/header.js
+++ myportfolio/js/header.js
@@ -3,7 +3,9 @@
         overlay = document.querySelector('.nav_overlay'),
         headerBar = document.querySelector('.header_bar_container'),
         headerLogo = document.querySelector('#header_logo'),
-        noScroll = document.querySelector('body');
+        noScroll = document.querySelector('body'),
+        backToTop = document.querySelector('.backToTop_wrap'),
+        deActive = document.querySelectorAll('.de-active');
 
     function toggleClass() {
         burger.classList.toggle('active');
@@ -12,10 +14,12 @@
     }
 
     function shrinkNav() {
-        if (window.pageYOffset > 80) {
+        if (window.pageYOffset > 100) {
+            backToTop.style.opacity = 1;
             headerBar.classList.add('shrink');
             headerLogo.src = 'images/shan-logo-3.0-name-black.svg';
         } else {
+            backToTop.style.opacity = 0;
             headerBar.classList.remove('shrink');
             headerLogo.src = 'images/shan-logo-3.0-name-white.svg';
         }
@@ -23,4 +27,7 @@
 
     burger.addEventListener('click', toggleClass, false);
     window.addEventListener('scroll', shrinkNav, false);
+    deActive.forEach(function (menu) {
+        menu.addEventListener('click', toggleClass, false);
+    });
 })();
---
+++ myportfolio/js/portfolioData.js
@@ -0,0 +1,41 @@
+var portfolioData = {
+    lotr: {
+        image: 'images/lotr.jpg',
+
+        alt: 'Lord of the Ring',
+
+        title: 'Lord Of The Ring 15th Anniversary',
+
+        content: 'It’s the turbocharged motoring icon, with more handles to love. The MINI 5 door boasts the same iconic good looks as its 3 door brother, but features an extra pair of passenger doors for practical access to its spacious cabin.'
+    },
+
+    crunican: {
+        image: 'images/crunican_adrian.jpg',
+
+        alt: 'Crunican Web Design',
+
+        title: 'Crunican Web Design',
+
+        content: 'A true style icon – reimagined for contemporary living. Paying tribute to the original 1959 Austin Seven, the MINI Seven Edition brings together our most distinctive elements into one model.'
+    },
+
+    logofolio: {
+        image: 'images/logofolio.jpg',
+
+        alt: 'Logofolio',
+
+        title: '2016-2017 Logofolio',
+
+        content: 'It’s almost time to re-discover the redesigned, re-engineered, and completely revamped MINI Convertible.'
+    },
+
+    photo: {
+        image: 'images/photography.jpg',
+
+        alt: 'Photography',
+
+        title: 'Photo Works',
+
+        content: 'It’s almost time to re-discover the redesigned, re-engineered, and completely revamped MINI Convertible.'
+    }
+};
