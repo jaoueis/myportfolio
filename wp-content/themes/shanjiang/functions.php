e3aff6637afa6c8a8b46082309d0846557914dc7
pan <panbanglanfeng@gmail.com>
2018-02-12 22:05:10 -0500

    enable theme support

--- wp-content/themes/shanjiang/functions.php
+++ wp-content/themes/shanjiang/functions.php
@@ -1,4 +1,20 @@
 <?php
+
+add_theme_support('post-thumbnails');
+set_post_thumbnail_size(125, 125, true);
+add_theme_support('automatic-feed-links');
+add_theme_support('menus');
+add_theme_support('title-tag');
+add_theme_support(
+    'html5',
+    array(
+        'comment-list',
+        'comment-form',
+        'search-form',
+    )
+);
+
+
 add_action('wp_enqueue_scripts', 'shanjiang_load_scripts');
 
 function shanjiang_load_scripts()
@@ -8,3 +24,4 @@ function shanjiang_load_scripts()
     wp_enqueue_style('foundation-app', get_template_directory_uri() . '/css/app.css');
     wp_enqueue_style('site-styles', get_template_directory_uri() . '/css/styles.css', array('foundation', 'foundation-app'));
 }
+
