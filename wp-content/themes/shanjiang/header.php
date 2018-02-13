15a29c19e278d16dbcb041b301c39839d8998308
pan <panbanglanfeng@gmail.com>
2018-02-12 22:13:28 -0500

    replace hardcoded menu by wp menu

--- wp-content/themes/shanjiang/header.php
+++ wp-content/themes/shanjiang/header.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-<?php wp_head();?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
@@ -16,7 +17,8 @@
     </div>
     <div class="logo_wrap small-4 medium-6 large-8 cell">
         <a href="http://shanjiang.io">
-            <img src="<?php echo get_template_directory_uri();?>/images/shan-logo-3.0-name-white.svg" alt="SHAN Logo" id="header_logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/shan-logo-3.0-name-white.svg" alt="SHAN Logo"
+                 id="header_logo">
         </a>
     </div>
     <div class="burger_button_wrap small-4 medium-3 large-2 cell">
@@ -29,15 +31,13 @@
     <div class="nav_overlay">
         <nav>
             <h2 class="hide">Main Nav</h2>
-            <ul data-magellan>
-                <li><a href="#backToTop" class="de-active">home</a></li>
-                <li><a href="#about" class="de-active">about</a></li>
-                <li><a href="#services" class="de-active">services</a></li>
-                <li><a href="#portfolio" class="de-active">portfolio</a></li>
-                <li><a href="#main_form" class="de-active">contact</a></li>
-                <li><a href="http://shanjiang.io/shan_jiang_resume.pdf" class="de-active" target="_blank">resume</a>
-                </li>
-            </ul>
+            <?php wp_nav_menu(
+                array(
+                    'menu' => '',
+                    'theme_location' => 'main-menu',
+                    'items_wrap' => '<ul data-magellan>%3$s</ul>',
+                )
+            ); ?>
         </nav>
     </div>
 </div>
