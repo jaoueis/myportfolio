b35b3760aba37faaa91f6c4cff3137299217fc94
Shan Jiang <jiang_shan@live.com>
2018-04-11 17:50:07 -0400

    update

--- wp-content/themes/shanjiang/index.php
+++ wp-content/themes/shanjiang/index.php
@@ -19,44 +19,12 @@ if ($query->have_posts()) {
         echo '<div class="portfolio_wrap cell medium-6"><div class="portfolio_overlay"><h3>' . get_the_title() . '</h3></div></div>';
     }
     echo '</div>';
-    /* Restore original Post Data */
+
     wp_reset_postdata();
 } else {
     echo "no posts found";
 }
 ?>
-
-    <div class="portfolio_wrap cell medium-6" id="lotr" data-roundaboutindex="0">
-        <div class="portfolio_overlay">
-            <h3>Lord Of The Ring 15th Anniversary</h3>
-        </div>
-    </div>
-    <div class="portfolio_wrap cell medium-6" id="crunican" data-roundaboutindex="1">
-        <div class="portfolio_overlay">
-            <h3>Crunican Website Design</h3>
-        </div>
-    </div>
-    <div class="portfolio_wrap cell medium-6" id="logofolio" data-roundaboutindex="2">
-        <div class="portfolio_overlay">
-            <h3>2016-2017 Logofolio</h3>
-        </div>
-    </div>
-    <div class="portfolio_wrap cell medium-6" id="photo" data-roundaboutindex="3">
-        <div class="portfolio_overlay">
-            <h3>Photo Works</h3>
-        </div>
-    </div>
-    <div class="portfolio_showboard grid-x">
-        <div class="portfolio_close">
-            <div class="top"></div>
-            <div class="bot"></div>
-        </div>
-        <div class="cell small-8 small-offset-2 large-6 large-offset-3 portfolio_content_wrap">
-            <img src="" alt="" class="portfolio_img">
-            <h4 class="portfolio_title"></h4>
-            <p class="portfolio_content"></p>
-        </div>
-    </div>
 
 <?php get_template_part('template-parts/content/content', 'strength'); ?>
 <?php get_template_part('template-parts/content/content', 'contact'); ?>
