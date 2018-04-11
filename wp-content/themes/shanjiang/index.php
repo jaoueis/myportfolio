d2888cfd91bec9d59cc32b96ba4255bcb7ad54e0
Shan Jiang <jiang_shan@live.com>
2018-04-11 17:26:54 -0400

    update

--- wp-content/themes/shanjiang/index.php
+++ wp-content/themes/shanjiang/index.php
@@ -4,12 +4,7 @@
 <?php get_template_part('template-parts/content/content', 'about'); ?>
 <?php get_template_part('template-parts/content/content', 'services'); ?>
 <?php get_template_part('template-parts/content/content', 'services-details'); ?>
-    <div class="portfolio_description cell medium-10 medium-offset-1">
-        <h2>portfolio</h2>
-        <div class="section_hr"></div>
-        <p>I have worked with a variety of different clients. Below is my recent development, design, and photography
-            works.</p>
-    </div>
+
 <?php
 
 // The Query
@@ -18,6 +13,7 @@ $query = new WP_Query(array('author' => 'admin'));
 // The Loop
 if ($query->have_posts()) {
     echo '<div class="portfolio_container grid-x" id="portfolio" data-magellan-target="portfolio">';
+    echo '<div class="portfolio_description cell medium-10 medium-offset-1"><h2>portfolio</h2><div class="section_hr"></div><p>I have worked with a variety of different clients. Below is my recent development, design, and photography works.</p></div>';
     while ($query->have_posts()) {
         $query->the_post();
         echo '<div class="portfolio_wrap cell medium-6"><a href="' . the_permalink() . '"><div class="portfolio_overlay"><h3>' . get_the_title() . '</h3></div></a></div>';
