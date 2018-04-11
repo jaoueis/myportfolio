ea11107dabbc9ce5766e36563fe68a2f10372555
Shan Jiang <jiang_shan@live.com>
2018-04-11 15:33:54 -0400

    test

--- wp-content/themes/shanjiang/single-portfolio.php
+++ wp-content/themes/shanjiang/single-portfolio.php
@@ -14,7 +14,7 @@ get_header(); ?>
              * use this in a child theme, then include a file called content-___.php
              * (where ___ is the post format) and that will be used instead.
              */
-            get_template_part('template_parts/post/content', 'none');
+            get_template_part('template-parts/post/content', 'none');
 
             // End the loop.
         endwhile;
