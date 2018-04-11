f7eddf09e7beed59b7108976b88b77a6c5da8043
Shan Jiang <jiang_shan@live.com>
2018-04-11 15:57:14 -0400

    update

--- wp-content/themes/shanjiang/template-parts/post/content.php
+++ wp-content/themes/shanjiang/template-parts/post/content.php
@@ -1,18 +1,8 @@
 <section class="grid-x portfolio-post-wrap">
     <div class="cell small-12 portfolio-post-banner-wrap">
-        <div class="portfolio-post-banner">
-            <h1>Portfolio Case Study Title</h1>
+        <div class="portfolio-post-banner" style="background: <?php the_post_thumbnail('full'); ?>; background-size: cover; background-position: center;">
+            <h1><?php the_title(); ?></h1>
         </div>
-    </div>
-
-    <div class="cell small-12 large-5 large-offset-1 portfolio-post-intro-img-wrap">
-        <div class="portfolio-post-intro-img">
-
-        </div>
-    </div>
-    <div class="cell small-12 large-5 portfolio-post-desc-wrap">
-        <h2>this is subtitle</h2>
-        <p>this is description</p>
     </div>
     <?php the_content(); ?>
 </section>
