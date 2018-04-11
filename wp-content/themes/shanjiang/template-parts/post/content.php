4e9a5f3cb19a1ae0a99f840fe34060b144606463
Shan Jiang <jiang_shan@live.com>
2018-04-11 16:23:25 -0400

    update

--- wp-content/themes/shanjiang/template-parts/post/content.php
+++ wp-content/themes/shanjiang/template-parts/post/content.php
@@ -2,7 +2,7 @@
     <div class="cell small-12 portfolio-post-banner-wrap">
         <?php $bg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
         <div class="portfolio-post-banner" style="background-image: url('<?php echo $bg[0]; ?>'); background-repeat: no-repeat; background-position: center; background-size: cover;">
-            <h1><?php the_title(); ?></h1>
+            <h3><?php the_title(); ?></h3>
         </div>
     </div>
     <?php the_content(); ?>
