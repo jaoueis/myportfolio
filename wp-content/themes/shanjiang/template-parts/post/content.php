3f3c626b68cc67b045e3ee9bb039cd8ee578abd4
Shan Jiang <jiang_shan@live.com>
2018-04-11 16:05:45 -0400

    update

--- wp-content/themes/shanjiang/template-parts/post/content.php
+++ wp-content/themes/shanjiang/template-parts/post/content.php
@@ -1,6 +1,6 @@
 <section class="grid-x portfolio-post-wrap">
     <div class="cell small-12 portfolio-post-banner-wrap">
-        <div class="portfolio-post-banner" style="background: <?php the_post_thumbnail('full'); ?>">
+        <div class="portfolio-post-banner" style="background: '<?php the_post_thumbnail('full'); ?>'">
             <h1><?php the_title(); ?></h1>
         </div>
     </div>
