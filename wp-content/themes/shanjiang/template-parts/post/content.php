e6fd6a80e1c3966f93a250da9d35b5c7034fdf4a
Shan Jiang <jiang_shan@live.com>
2018-04-11 15:28:47 -0400

    added single-potfolio

---
+++ wp-content/themes/shanjiang/template-parts/post/content.php
@@ -0,0 +1,18 @@
+<section class="grid-x portfolio-post-wrap">
+    <div class="cell small-12 portfolio-post-banner-wrap">
+        <div class="portfolio-post-banner">
+            <h1>Portfolio Case Study Title</h1>
+        </div>
+    </div>
+
+    <div class="cell small-12 large-5 large-offset-1 portfolio-post-intro-img-wrap">
+        <div class="portfolio-post-intro-img">
+
+        </div>
+    </div>
+    <div class="cell small-12 large-5 portfolio-post-desc-wrap">
+        <h2>this is subtitle</h2>
+        <p>this is description</p>
+    </div>
+    <?php the_content(); ?>
+</section>
