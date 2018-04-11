0299d370e1becc4be9c3307229ccc24b39a5b331
Shan Jiang <jiang_shan@live.com>
2018-04-11 16:16:57 -0400

    update

--- wp-content/themes/shanjiang/template-parts/post/content.php
+++ wp-content/themes/shanjiang/template-parts/post/content.php
@@ -1,7 +1,7 @@
 <section class="grid-x portfolio-post-wrap">
     <div class="cell small-12 portfolio-post-banner-wrap">
         <?php $bg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
-        <div class="portfolio-post-banner" style="background-image: url(<?php echo $bg; ?>); background-repeat: no-repeat; background-position: center; background-size: cover;">
+        <div class="portfolio-post-banner" style="background-image: url('<?php echo $bg[0]; ?>'); background-repeat: no-repeat; background-position: center; background-size: cover;">
             <h1><?php the_title(); ?></h1>
         </div>
     </div>
