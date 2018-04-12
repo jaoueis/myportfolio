e56a11f9c0d0e638b83eb6ed266036cf305386a8
Shan Jiang <jiang_shan@live.com>
2018-04-12 01:46:01 -0400

    changes

--- wp-content/themes/shanjiang/template-parts/post/content.php
+++ wp-content/themes/shanjiang/template-parts/post/content.php
@@ -1,4 +1,4 @@
-<section class="grid-x portfolio-post-wrap">
+<section class="grid-x portfolio-post-wrap" id="backToTop" data-magellan-target="backToTop">
     <div class="cell small-12 portfolio-post-banner-wrap">
         <?php $bg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
         <div class="portfolio-post-banner" style="background-image: url('<?php echo $bg[0]; ?>'); background-repeat: no-repeat; background-position: center; background-size: cover;">
