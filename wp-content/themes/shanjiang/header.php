2721a1f80daf5396abed7276bfb45777b4430e08
Shan Jiang <jiang_shan@live.com>
2018-05-11 00:12:30 -0400

    updated links

--- wp-content/themes/shanjiang/header.php
+++ wp-content/themes/shanjiang/header.php
@@ -11,10 +11,10 @@
 <h1 class="hide">S H A N</h1>
 <div class="header_bar_container grid-x">
     <div class="title_wrap small-4 medium-3 large-2 cell" data-magellan>
-        <a href="#main_form"><p class="fa fa-envelope-o"></p></a>
+        <a href="http://shanjiang.io/#main_form"><p class="fa fa-envelope-o"></p></a>
     </div>
     <div class="logo_wrap small-4 medium-6 large-8 cell">
-        <a href="http://wp.shanjiang.io">
+        <a href="http://shanjiang.io">
             <img src="<?php echo get_template_directory_uri(); ?>/images/shan-logo-3.0-name-white.svg" alt="Shan Logo" id="header_logo" data-src-black="<?php echo get_template_directory_uri(); ?>/images/shan-logo-3.0-name-black.svg" data-src-white="<?php echo get_template_directory_uri(); ?>/images/shan-logo-3.0-name-white.svg">
         </a>
     </div>
